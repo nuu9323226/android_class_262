05b2ef786907313de5ed30ed5061e60a96ac08f5
nuu9323226 <nuu9323226@gmail.com>
2015-11-26 22:00:11 +0800

    class1126
    bottun,event

--- SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/simpleui/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.TooManyListenersException;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText inputText;
+    //private View view;
+    private CheckBox hideCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputText = (EditText)findViewById(R.id.editText);
+        inputText.setText("1234");
+        inputText.setOnKeyListener(new View.OnKeyListener() {
+                                       public boolean onKey(View v, int KeyCode, KeyEvent event) {
+                                           if (event.getAction() == KeyEvent.ACTION_DOWN) {
+                                               if (KeyCode == KeyEvent.KEYCODE_ENTER) {
+                                                   submit(v);
+                                                   return true;
+                                               }
+                                           }
+                                           return false;
+                                       }
+                                   });
+        hideCheckBox = (CheckBox) findViewById(R.id.hideCheckBox);
+       // hideCheckBox.setChecked(true);
     }
+    public void submit(View view){
+        //this.view = view;
+        String text = inputText.getText().toString();
+        if(hideCheckBox.isChecked()){
+            text = "**********";
+            inputText.setText("***********");
+        }
+
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+
+
+
 }
